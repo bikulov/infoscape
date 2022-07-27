14ba1972ea5137462b95ac12a5590d920ed50be9
Dmitry Bikulov <ksar@bikulov.org>
2022-07-27 18:01:26 +0300

    Self-written CSS + js for popups

---
+++ app/static/popup.js
@@ -0,0 +1,13 @@
+function show_popup(el) {
+    var pos = el.getBoundingClientRect();
+    var popup = document.getElementById("popup");
+
+    popup.innerHTML = el.dataset.post;
+    popup.style.display = "block";
+    popup.style.left = pos.left + "px";
+    popup.style.top = pos.bottom + "px";
+}
+
+function hide_popup(el) {
+    document.getElementById("popup").style.display = "none";
+}
